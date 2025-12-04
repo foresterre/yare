9d9b8628456fd60cd72e5b42ca89a68a98664848
Martijn Gribnau <garm@ilumeo.com>
2025-12-05 00:40:04 +0100

    Extract parameterized test function to the greenhouse crate
    The same functionality is used by the 'parameterized', 'yare' and 'sif' macro crates.

--- yare-macro/src/lib.rs
+++ yare-macro/src/lib.rs
@@ -3,12 +3,11 @@
 //! Documentation can be found [here](https://github.com/foresterre/yare).
 #![deny(clippy::all)]
 
+extern crate proc_macro;
 #[macro_use]
 extern crate syn;
-extern crate proc_macro;
 
 mod test_cases;
-mod test_fn;
 
 #[proc_macro_attribute]
 pub fn parameterized(
@@ -16,7 +15,7 @@ pub fn parameterized(
     input: ::proc_macro::TokenStream,
 ) -> ::proc_macro::TokenStream {
     let test_cases = parse_macro_input!(args as test_cases::TestCases);
-    let test_fn = parse_macro_input!(input as test_fn::TestFn);
+    let test_fn = parse_macro_input!(input as greenhouse::TestFn);
 
     test_cases
         .to_token_stream(&test_fn)
